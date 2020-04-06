1c50a43b53d5b4dfd4a0129a006690b1c6a2142b
tarunbhatt9784 <32237362+tarunbhatt9784@users.noreply.github.com>
2020-04-06 23:15:53 +1000

    Draft version

---
+++ Yield/Yield/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Yield
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CallFibonacci();
+        }
+
+
+        public static void CallFibonacci()
+        {
+            /*
+             *GetFibonacciNumber method is called. 
+             * Parameter passed is 5, hence GetFibonacciNumber will return 5 fibinacci numbers
+             */
+            var numbers = GetFibonacciNumber(100); // 1
+            foreach (var no in numbers) // 2
+            {
+                Console.WriteLine(no); 
+            }
+        }
+    
+    public static IEnumerable<int> GetFibonacciNumber(int count)
+        {
+            int prev = -1; //3
+            int next = 1;
+            for (int index = 0; index < count; index++)
+            {
+                int sum = prev + next;
+                prev = next;
+                next = sum;
+                yield return sum; // 4
+            }
+        }
+    }
+}
